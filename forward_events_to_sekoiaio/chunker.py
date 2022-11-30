c1ee226a087dc64eeb5b2f725eb11753aa9128f0
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2022-11-30 10:53:13 +0100

    feat: Limit the size of request to the HTTP collector of SEKOIA.IO (to avoid HTTP 413 Payload Too Large response)

--- forward_events_to_sekoiaio/chunker.py
+++ forward_events_to_sekoiaio/chunker.py
@@ -1,13 +1,17 @@
+from forward_events_to_sekoiaio.constants import CHUNK_BYTES_MAX_SIZE
+
+
 class Chunker:
     """
     Split a sequence of events into fixed-size of chunks
     """
 
-    def __init__(self, chunk_size):
+    def __init__(self, chunk_size, bytes_max_size=CHUNK_BYTES_MAX_SIZE):
         """
         :param int chunk_size: The size of chunks
         """
-        self.chunk_size = chunk_size
+        self.__chunk_size = chunk_size
+        self.__bytes_max_size = bytes_max_size
 
     def chunk(self, events):
         """
@@ -16,18 +20,24 @@ class Chunker:
         :param Sequence events: The list of events to split
         """
         chunk: list = []
+        chunk_bytes = 0
 
         # iter over the events
         for event in events:
 
-            # if the chnuk is full
-            if len(chunk) >= self.chunk_size:
+            # if the chunk is full or exceed maximal chunk size
+            if (
+                len(chunk) >= self.__chunk_size
+                or chunk_bytes + len(event) > self.__bytes_max_size
+            ):
                 # yield the current chunk and create a new one
                 yield chunk
                 chunk = []
+                chunk_bytes = 0
 
             # add the event to the current chunk
             chunk.append(event)
+            chunk_bytes += len(event)
 
         # if the last chunk is not empty
         if len(chunk) > 0:
--- forward_events_to_sekoiaio/constants.py
+++ forward_events_to_sekoiaio/constants.py
@@ -1,1 +1,4 @@
 SEKOIAIO_INTAKE_HOST = "https://intake.sekoia.io"
+JSON_ENVELOP_BYTES = len('{"intake_key":"","jsons":[]}') + 36  # intake key size
+INTAKE_PAYLOAD_BYTES_MAX_SIZE = 10485760  # 10Miobytes
+CHUNK_BYTES_MAX_SIZE = INTAKE_PAYLOAD_BYTES_MAX_SIZE - JSON_ENVELOP_BYTES
